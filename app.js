b5e3930944f5b30f4283b4f732ca2bae2b405b11
ianmarcel <ianmarcelcampos.com>
2024-07-10 10:42:22 -0300

    aula 5 - Seção 12

---
+++ app.js
@@ -0,0 +1,30 @@
+const container        = document.querySelector(".container");
+const lista_atividades = document.querySelector(".lista_atividades");
+const input            = document.querySelector(".input");
+const erro             = document.querySelector(".erro");
+const botaoCadastro    = document.querySelector(".botao_adc");
+const paleta1          = document.querySelector("#paleta1");
+const paleta2          = document.querySelector("#paleta2");
+const paleta3          = document.querySelector("#paleta3");
+
+
+function cadastraAtividade(){
+    if(input.value.length > 3){
+        erro.style.display = "none";
+        //criaAtividade();
+    }else{
+        erro.style.display = "grid";
+        erro.innerHTML = `${input.value} não é uma atividade válida!`
+    }
+    limpaInput();
+}
+
+function limpaInput(){
+    input.value = "";
+}
+
+window.addEventListener("keypress", (e) => {
+    if(e.key === "Enter"){
+        cadastraAtividade();
+    }
+});
